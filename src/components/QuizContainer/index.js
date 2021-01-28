272c8f8be0c0420158628927d131cda039e34bba
pdrolucasz <pedro.lucas4431@gmail.com>
2021-01-28 01:14:29 -0300

    componentes de button, input, QuizContainer e widget de tópico para alternativas

---
+++ src/components/QuizContainer/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable linebreak-style */
+import styled from 'styled-components';
+
+const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 130px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 180px 15px 15px;
+  }
+`;
+
+export default QuizContainer;
