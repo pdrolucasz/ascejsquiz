be05e1962baf62f5a782596418d4627388fbd831
pdrolucasz <pedro.lucas4431@gmail.com>
2021-01-30 21:20:35 -0300

    link desabilidado caso usuário não tenha colocado nome

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
+import { motion } from 'framer-motion';
+
 import db from '../db.json';
 
 import Widget from '../src/components/Widget';
@@ -29,7 +31,16 @@ export default function Home() {
   return (
     <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>{db.title}</h1>
           </Widget.Header>
@@ -48,7 +59,16 @@ export default function Home() {
           </Widget.Content>
         </Widget>
 
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>Quizes da Galera</h1>
           </Widget.Header>
@@ -62,7 +82,10 @@ export default function Home() {
                   .split('.');
                 return (
                   <li key={linkExterno}>
-                    <Widget.Topic as={Link} href={`/quiz/${projectName}___${githubUser}`}>
+                    <Widget.Topic
+                      as={Link}
+                      href={name.length === 0 ? '/' : `/quiz/${projectName}___${githubUser}?name=${name}`}
+                    >
                       {`${githubUser}${projectName}`}
                     </Widget.Topic>
                   </li>
